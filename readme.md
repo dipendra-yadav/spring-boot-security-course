108af501b6df76a512041e877562d91924242d4a
dipendra-yadav <dipendra.eng@gmail.com>
2020-03-07 12:58:54 +0530

    commit.

---
+++ readme.md
@@ -0,0 +1,2 @@
+simple API returns Model Data.
+//https://www.youtube.com/watch?v=her_7pa0vrg
--- src/main/java/com/example/demo/student/Student.java
+++ src/main/java/com/example/demo/student/Student.java
@@ -2,20 +2,19 @@ package com.example.demo.student;
 
 public class Student {
 
-    private final Integer studentId;
-    private final String studentName;
+	private final Integer studentId;
+	private final String studentName;
 
-    public Student(Integer studentId,
-                   String studentName) {
-        this.studentId = studentId;
-        this.studentName = studentName;
-    }
+	public Student(Integer studentId, String studentName) {
+		this.studentId = studentId;
+		this.studentName = studentName;
+	}
 
-    public Integer getStudentId() {
-        return studentId;
-    }
+	public Integer getStudentId() {
+		return studentId;
+	}
 
-    public String getStudentName() {
-        return studentName;
-    }
+	public String getStudentName() {
+		return studentName;
+	}
 }
